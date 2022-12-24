48e75f9c7a77b43fa30256275836afb3d1995572
Colton Harmon <cmharmon89@gmail.com>
2022-12-24 09:39:34 -0800

    Early Basic Buster Vance Site

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # busterVance
+
+Credit:
+
+Sky-Bus picture taken from this website:
+https://www.greatbasin.org/81/welcome-to-the-future/
+
+Starlink picture taken from this website:
+https://spacenews.com/spacex-warns-5g-plan-would-deny-starlink-to-most-americans/
+
+map from:
+https://www.roamingaroundtheworld.com/us-roadtrip-best/
---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+$(function() {
+    $(".carousel").carousel( { interval: 3000, pause: "false" } );
+    $("#carouselButton").click(function(){
+        if ($("#carouselButton").children("i").hasClass("fa-pause")) {
+            $(".carousel").carousel("pause");
+            $("#carouselButton").children("i").removeClass("fa-pause");
+            $("#carouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".carousel").carousel("cycle");
+            $("#carouselButton").children("i").removeClass("fa-play");
+            $("#carouselButton").children("i").addClass("fa-pause");
+        }
+    });
+});
+
+$(function() {
+    $(".galleryCarousel").carousel( { interval: 3000, pause: "false" } );
+    $("#galleryCarouselButton").click(function(){
+        if ($("#galleryCarouselButton").children("i").hasClass("fa-pause")) {
+            $(".galleryCarousel").carousel("pause");
+            $("#galleryCarouselButton").children("i").removeClass("fa-pause");
+            $("#galleryCarouselButton").children("i").addClass("fa-play");
+        } else {
+            $(".galleryCarousel").carousel("cycle");
+            $("#galleryCarouselButton").children("i").removeClass("fa-play");
+            $("#galleryCarouselButton").children("i").addClass("fa-pause");
+        }
+    });
+});
+
+$(function() {
+    $("#subscribeButton").click(function() {
+        $("#subscribeModal").modal("show");
+    });
+});
+
+$(function() {
+    $("#navSubButton").click(function() {
+        $("#subscribeModal").modal("show");
+    });
+});
+
+$(function() {
+    $("#navGalBut").click(function() {
+        $("#galleryModal").modal("show");
+    });
+});
+
+$(function() {
+    $("#presentGalleryButton").click(function() {
+        $("#galleryModal").modal("show");
+    });
+});
+
+
